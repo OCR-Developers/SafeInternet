fb20b53dc2ca090c84b48e74aed1121184b3b36f
Al Mamun <43331991+almamuncse@users.noreply.github.com>
2020-03-10 19:51:37 +0600

    Revert "delete toast"

--- app/src/main/java/com/example/safeinternetwebview/MainActivity.java
+++ app/src/main/java/com/example/safeinternetwebview/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(this, "hekk", Toast.LENGTH_SHORT).show();
 
     }
 }
