1c37e47a18c3b8b7879ae8873c5a39c42be0646e
Al Mamun <43331991+almamuncse@users.noreply.github.com>
2020-03-10 19:05:31 +0600

    Initailize commit

---
+++ app/src/main/java/com/example/safeinternetwebview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.safeinternetwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started.");
+
+        webView=findViewById(R.id.webview);
+
+        //enable javaScript
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://safeinternet.unicefbangladesh.org/");
+    }
+}
