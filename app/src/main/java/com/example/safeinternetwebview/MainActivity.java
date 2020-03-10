0758ab2d129ce40e013051f0afa3fcd1367877fd
developer-masud-rana <masudrana35362@gmail.com>
2020-03-10 19:21:48 +0600

    masud all code delete

--- app/src/main/java/com/example/safeinternetwebview/MainActivity.java
+++ app/src/main/java/com/example/safeinternetwebview/MainActivity.java
@@ -1,32 +1,17 @@
 package com.example.safeinternetwebview;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
-    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: started.");
 
-        webView=findViewById(R.id.webview);
-
-        //enable javaScript
-        WebSettings webSettings=webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        webView.loadUrl("https://safeinternet.unicefbangladesh.org/");
-        Toast.makeText(this, "hekklo", Toast.LENGTH_SHORT).show();
 
     }
 }
