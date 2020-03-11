99da9a53a1d3096054a0286d511b100b2b1d2884
Al Mamun CSE <almamuncse2000@gmail.com>
2020-03-11 14:41:30 +0600

    MainActivity

--- app/src/main/java/com/example/safeinternetwebview/MainActivity.java
+++ app/src/main/java/com/example/safeinternetwebview/MainActivity.java
@@ -19,14 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: started.");
 
-        webView=findViewById(R.id.webview);
-
-        //enable javaScript
-        WebSettings webSettings=webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        webView.loadUrl("https://safeinternet.unicefbangladesh.org/");
-
-        Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+        //Master Branch MainActivity
     }
 }
