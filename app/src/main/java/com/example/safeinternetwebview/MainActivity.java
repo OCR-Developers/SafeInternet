f3dfac96cb09f3e365ba6d9034e012d92c41ce7f
developer-masud-rana <masudrana35362@gmail.com>
2020-03-11 14:00:01 +0600

    QuizShowActivity with right and wrong dialogue layout design

--- app/src/main/java/com/example/safeinternetwebview/MainActivity.java
+++ app/src/main/java/com/example/safeinternetwebview/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.safeinternetwebview;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,10 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(this, "hekk", Toast.LENGTH_SHORT).show();
-
-        Toast.makeText(this, "a;sdjf;aslkdjf", Toast.LENGTH_SHORT).show();
 
     }
 }
---
+++ app/src/main/java/com/example/safeinternetwebview/QuizShowActivity.java
@@ -0,0 +1,16 @@
+package com.example.safeinternetwebview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_show);
+
+
+    }
+}
